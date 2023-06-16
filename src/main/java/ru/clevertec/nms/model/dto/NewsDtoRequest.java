dd6eb699d3eed6a386489973c5e371d64bc7e552
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-06-16 12:47:45 +0300

    feat: add dtos

---
+++ src/main/java/ru/clevertec/nms/model/dto/NewsDtoRequest.java
@@ -0,0 +1,22 @@
+package ru.clevertec.nms.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class NewsDtoRequest {
+
+    private String title;
+    private String text;
+}
