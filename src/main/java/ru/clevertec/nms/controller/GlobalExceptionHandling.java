31d53b82096d1bcd646f13472af11718d5088551
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-07-02 03:00:21 +0300

    feat: add logging to exception handling

--- src/main/java/ru/clevertec/nms/controller/GlobalExceptionHandling.java
+++ src/main/java/ru/clevertec/nms/controller/GlobalExceptionHandling.java
@@ -1,6 +1,9 @@
 package ru.clevertec.nms.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,16 +13,23 @@ import ru.clevertec.nms.model.exception.ExceptionInfo;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandling extends ResponseEntityExceptionHandler {
-
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex, WebRequest request) {
 
-        return buildResponseEntity(buildExceptionInfo(HttpStatus.BAD_REQUEST, ex, request));
+        ExceptionInfo exceptionInfo = buildExceptionInfo(HttpStatus.BAD_REQUEST, ex, request);
+        log.error(exceptionInfo.toString());
+        return buildResponseEntity(exceptionInfo);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error(buildExceptionInfo(HttpStatus.valueOf(statusCode.value()), ex, request).toString());
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
 
     private ExceptionInfo buildExceptionInfo(HttpStatus httpStatus, Exception ex, WebRequest request) {
         return ExceptionInfo.builder()
--- src/main/java/ru/clevertec/nms/model/exception/ExceptionInfo.java
+++ src/main/java/ru/clevertec/nms/model/exception/ExceptionInfo.java
@@ -2,12 +2,14 @@ package ru.clevertec.nms.model.exception;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@ToString
 public class ExceptionInfo {
 
     private final LocalDateTime timestamp = LocalDateTime.now();
